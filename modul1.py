510d0ed7bd6ab0a949a4896b93ce0d74e1a15be7
Miriam <miriam152000@yahoo.com>
2021-04-14 21:27:51 +0300

    add module 1

---
+++ modul1.py
@@ -0,0 +1,22 @@
+print('Hello world')
+print('print\u200b("")')
+# print​("")
+Hello_World = 'Hello World'
+print(Hello_World)
+
+name = 'Miriam'
+# print(3 * (name + " "))
+# print(name,name,name, sep='#', end='')
+#print(100 * (name + "\n"))
+print(name * 3)
+print(3* name)
+print(print)
+print('   *  ')
+print('  *** ')
+print(' ****** ')
+print('\n')
+print(3 * ('*******\n'))
+
+print(' _____ ')
+print('|     |')
+print('|_____|')
