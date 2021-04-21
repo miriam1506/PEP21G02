ccf4ed192247994107a325d9b0d5872614f07427
Miriam <miriam152000@yahoo.com>
2021-04-21 21:00:40 +0300

    add module 2

--- modul2/app4.py
+++ modul2/app4.py
@@ -40,3 +40,17 @@ for letter in result[1:]:
 
 print(result2)
 
+a = 'a'
+b = 'b'
+print(a and b)
+if a:
+    print(b)
+else:
+    print(a)
+a = False
+b = True
+print(a or b)
+if a:
+    print(a)
+else:
+    print(b)
